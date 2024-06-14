5def3681abdb728f0641d04be9b87912195892a7
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-14 12:52:19 -0500

    Actualiza ejercicio 4 de estructuras repetitivas: agrega una validación si se introducen números negativos como entrada y como salida se le pide que ingrese un número valido además de la opción de seguir con el programa o no

--- gastosviaje.cpp
+++ gastosviaje.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double gasto;
+    double suma = 0;
+    char continuar;
+    do {
+        cout << "Introduce un gasto: ";
+        cin >> gasto;
+        suma =suma + gasto;
+        cout << "Deseas continuar ingresando gastos? (s/n): ";
+        cin >> continuar;
+    } while (continuar == 's' || continuar == 'S');
+    cout << "La suma total de los gastos es: " << suma << endl;
+    return 0;
+}
 
