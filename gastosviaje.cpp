46e96347786fac353b59c13542f82ebe2075e878
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-14 12:56:34 -0500

    Actualiza ejercicio 4 debido a un error no se pudieron subir los cambios ahora si están presentes

--- gastosviaje.cpp
+++ gastosviaje.cpp
@@ -7,7 +7,13 @@ int main() {
     do {
         cout << "Introduce un gasto: ";
         cin >> gasto;
+        if (gasto<0){
+        	cout<<" Gasto invalido pruebe otro "<<endl;
+		}
         suma =suma + gasto;
+        if (suma<0){
+        	suma=0;
+		}
         cout << "Deseas continuar ingresando gastos? (s/n): ";
         cin >> continuar;
     } while (continuar == 's' || continuar == 'S');
