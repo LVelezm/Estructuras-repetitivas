29917d199923736fe1f1d3280329e27c24c65765
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-16 13:30:51 -0500

    Actualiza ejercicio 10 de estructuras repetitivas: Agrega un menú con dos opciones una de inicio y otra de salida si se coloca otra opción no será considerada

--- Nprimos.cpp
+++ Nprimos.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int N, i, cantidadDivisores;
-    cout << "Ingrese un numero: ";
-    cin >> N;
-    cantidadDivisores = 0;
-    if (N <= 1) {
-        cout << N << " no es primo." << endl;
-    } else {
-        i = 2;
-        while (i * i <= N && cantidadDivisores == 0) {
-            if (N % i == 0) {
+    int N, i, cantidadDivisores, opcion;
+    cout << "Este programa determinara si un numero es primo o no (1 o 2)"<<endl;
+    cout << "1. Iniciar "<<endl;
+	cout << "2. Salir "<<endl;
+	cin >> opcion;
+	if (opcion ==1){
+		    cout << "Ingrese un numero: ";
+   			cin >> N;
+    		cantidadDivisores = 0;
+    		if (N <= 1) {
+        		cout << N << " no es primo." << endl;
+    		} else {
+        		i = 2;
+        		while (i * i <= N && cantidadDivisores == 0) {
+            	if (N % i == 0) {
                 cantidadDivisores++;
-            }
-            i++;
-        }  
-        if (cantidadDivisores == 0) {
-            cout << N << " es primo." << endl;
-        } else {
-            cout << N << " no es primo." << endl;
-        }
-    }
-    return 0;
+            	}
+            		i++;
+        		}  
+        		if (cantidadDivisores == 0) {
+            		cout << N << " es primo." << endl;
+        		} else {
+            		cout << N << " no es primo." << endl;
+        		}
+    		}
+    		return 0;
+		}else{
+			if (opcion==2){
+			}else{
+				cout<<"opcion invalida"<<endl;
+			}
+		}	
 }
 
