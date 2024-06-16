dda94d80380d33f2207b4f94423daa9d363db582
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-15 19:30:58 -0500

    actualiza ejercicio 9 de estructuras repetitivas: Se agregó un menú que tiene dos opciones las cuales son iniciar el programa o salir, sin en dado caso se ingresa otro número el menú dice inválido

--- Cdigitos+.cpp
+++ Cdigitos+.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int numero;
+    int numero, opcion;
     int contador_digitos = 0;
-    cout << "Ingrese un numero positivo: ";
-    cin >> numero;
-    if (numero <= 0) {
-        cout << "El numero ingresado no es valido. Debe ser un numero positivo." << endl;
-        return 1;
-    }
-    do {
-        numero =numero/10;
-        contador_digitos++;
-    } while (numero != 0);
+    cout<<"Elija con 1 o 2"<<endl;
+    cout<<"1. Iniciar" <<endl;
+    cout<<"2. Salir" <<endl;
+    cin>>opcion;
+    if(opcion==1){
+    	    cout << "Ingrese un numero positivo: ";
+    		cin >> numero;
+    		if (numero <= 0) {
+        	cout << "El numero ingresado no es valido. Debe ser un numero positivo." << endl;
+        	return 1;
+    		}
+    	do {
+        	numero =numero/10;
+        	contador_digitos++;
+    		} while (numero != 0);
+    		cout << "El numero ingresado tiene " << contador_digitos << " digitos." << endl;
+			return 0;
+	}else{
+		if (opcion==2){
+			
+		}else{
+			if(opcion!= 1 || opcion!=2){
+				cout<<"opcion invalida";
+			}
+		}
+	}
 
-    cout << "El numero ingresado tiene " << contador_digitos << " digitos." << endl;
-
-    return 0;
 }
 
 
