8bacec9994e50db835c5ade70aac41756d2eac46
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-17 12:53:36 -0500

    Actualiza ejercicio 11 de estructuras repetitivas: Añade un menú de opciones para iniciar el programa

--- factoriales.cpp
+++ factoriales.cpp
@@ -3,9 +3,14 @@ using namespace std;
 int main() {
     int numero;
     int factorial = 1;
-    int i;
-    cout << "Ingrese un numero entero positivo: ";
-    cin >> numero;
+    int i, opcion;
+    cout<<"Este programa calculara el factorial de el numero ingresado (seleccione 1 o 2)"<<endl;
+    cout<<"1. Iniciar "<<endl;
+    cout<<"2. Salir "<<endl;
+    cin>>opcion;
+    if (opcion==1){
+    	cout << "Ingrese un numero entero positivo: ";
+    	cin >> numero;
     if (numero < 0) {
         cout << "El numero ingresado no es valido. Debe ser un entero positivo." << endl;
         return 1;
@@ -15,5 +20,12 @@ int main() {
     }
     cout << "El factorial de " << numero << " es: " << factorial << endl;
     return 0;
+	}else{
+		if (opcion==2){
+			return 0;
+		}else{
+			cout<<"opcion invalida"<<endl;
+		}
+	}
 }
 
