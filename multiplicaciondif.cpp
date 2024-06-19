ac939063f79ba8f465eec7c23c9b6d08035359ef
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-18 22:32:12 -0500

    Actualiza ejercicio 13 de estructuras repetitivas: Añade un menú para iniciar el programa o salir además si se ingresa otra opción esta figura como inválida

--- multiplicaciondif.cpp
+++ multiplicaciondif.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int num1, num2;
+    int num1, num2, opcion;
     int resultado = 0;
     int i;
+    cout<<"Este programa multiplicara sin usar * en c++"<<endl;
+	cout<<"1. Iniciar"<<endl;
+    cout<<"2. Salir"<<endl;
+    cin>>opcion;
+    if (opcion==1){
     cout << "Ingrese el primer numero a multiplicar: ";
     cin >> num1;
     cout << "Ingrese el segundo numero para multiplicar al primero: ";
@@ -14,5 +19,12 @@ int main() {
 
     cout << "El resultado de " << num1 << " x " << num2 << " es: " << resultado << endl;
     return 0;
+	}else{
+		if (opcion==2){
+			return 0;
+		}else{
+			cout<<"Opcion invalida"<<endl;
+		}
+	}
 }
 
