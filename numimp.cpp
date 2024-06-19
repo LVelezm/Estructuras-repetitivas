e616ee19780eb97e4bb55950b58201e39d2dcf9d
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-19 10:53:11 -0500

    Actualiza ejercicio 15 de estrcturas repetitivas: Añade un menú para iniciar o cerrar el programa si en dado caso se introdujera otra opcion figuraría como inválida

--- numimp.cpp
+++ numimp.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
+    int n, opcion;
     int suma = 0;
     int i;
+    cout<<"Este programa sumara los numeros impares hasta el numero ingresado"<<endl;
+	cout<<"1. Iniciar"<<endl;
+    cout<<"2. Salir"<<endl;
+    cin>>opcion;
+    if (opcion==1){
     cout << "Ingrese un numero entero positivo: ";
     cin >> n;
     if (n <= 0) {
@@ -15,5 +20,13 @@ int main() {
     }
     cout << "La suma de todos los numeros impares hasta " << n << " es: " << suma << endl;
     return 0;
+	}else{
+		if (opcion==2){
+			return 0;
+		}else{
+			cout<<"Opcion invalida"<<endl;
+		}
+	}
 }
+	
 
